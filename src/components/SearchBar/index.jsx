8ac719da16fca4e8ebb8bc15d7dc71d9b4d18014
Zeeeeeeju <QUENTIN.LIANG@OOCL.COM>
2020-08-10 17:52:42 +0800

    [front-end][Story 2][Quentin & Albert & Gavin] feature: modify disabledRangeTime function

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -2,22 +2,31 @@ import React from 'react';
 import SearchCity from '../SearchCity';
 import CityPosition from '../CityPosition';
 import { SearchOutlined } from '@ant-design/icons';
-import { Space, Button } from 'antd';
+import { Space, Button, DatePicker } from 'antd';
+import moment from 'moment';
+
+const { RangePicker } = DatePicker;
 
 class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			isDisableSearchButton: true,
+			isChooseDestination: false,
+			isChooseDate: false,
 			city: '',
-			destination: {}
+			destination: {},
+			currentHour: null
 		};
 	}
 
 	chooseDestination = (data) => {
-		this.setState({
-			destination: data
-		});
+		this.setState(
+			(state) => ({ destination: data, isChooseDestination: true }),
+			() => {
+				this.isComplete();
+			}
+		);
 		console.log(data);
 	};
 
@@ -27,16 +36,69 @@ class SearchBar extends React.Component {
 		});
 	};
 
+	onChange = (dates, dateStrings) => {
+		this.setState(
+			(state) => ({ isChooseDate: true }),
+			() => {
+				this.isComplete();
+			}
+		);
+	};
+
+	onOk = (data) => {
+		this.setState({ chooseDate: data });
+	};
+
+	isComplete() {
+		if (this.state.isChooseDate && this.state.isChooseDestination)
+			this.setState({
+				isDisableSearchButton: false
+			});
+	}
+
+	range(start, end) {
+		const result = [];
+		for (let i = start; i < end; i++) {
+			result.push(i);
+		}
+		return result;
+	}
+
+	disabledDate(current) {
+		return current < moment().startOf('days');
+	}
+
+	disabledRangeTime = (current, type) => {
+		if (type === 'start' && current !== null) {
+			this.state.currentHour = current.hours();
+			return {
+				disabledHours: () => this.range(0, 60).splice(0, moment().hours())
+			};
+		}
+
+		if (type === 'end' && current !== null) {
+			if (current.toDate().toDateString() === moment().toDate().toDateString()) {
+				return {
+					disabledHours: () => this.range(0, 60).splice(0, this.state.currentHour + 1)
+				};
+			}
+		}
+	};
+
 	render() {
 		return (
 			<Space>
 				<CityPosition chooseCity={this.chooseCity} />
 				<SearchCity chooseDestination={this.chooseDestination} city={this.state.city} />
-				<Button
-					type="primary"
-					icon={<SearchOutlined />}
-					disabled={this.state.isDisableSearchButton}
-				>
+				<RangePicker
+					disabledDate={this.disabledDate}
+					disabledTime={this.disabledRangeTime}
+					showTime={{}}
+					format="YYYY-MM-DD HH:00:00"
+					onChange={this.onChange}
+					onOk={this.onOk}
+				/>
+				<Button type="primary" icon={<SearchOutlined />} disabled={this.state.isDisableSearchButton}>
 					Search
 				</Button>
 			</Space>
