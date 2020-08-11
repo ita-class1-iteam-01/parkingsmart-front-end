a8a70114c2091efc63a1f31d43a5c6f5bdf6b938
xuxiaoqi-xxq <xxq_work@163.com>
2020-08-11 14:28:11 +0800

    [ST13][Eva] feature: add front-end  navigator ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import SearchBar from './components/SearchBar'
+import Navigator from './components/Navigator'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Navigator />
       <SearchBar />
     </div>
   )
