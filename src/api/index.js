9bc3cee5b47c8196309d7b90e1d1a7e89078fe06
haohao6683 <852721803@qq.com>
2020-08-12 14:09:40 +0800

    [Quentin & Draymond] Bug: fix some error about ESlint.

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 
 const baseURL = '/api'
 
-export const getBookOrderById  = function (id) {
+export const getBookOrderById  = (id) => {
   return axios.get(`${baseURL}/bookOrders/${id}`)
 }
 
-export const newBookOrder  = function (bookOrder) {
+export const newBookOrder  = (bookOrder) => {
   return axios.post(`${baseURL}/bookOrders`, bookOrder)
 }
