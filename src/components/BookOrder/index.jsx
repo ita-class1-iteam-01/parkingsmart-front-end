58005c732f7030c2eebc3ee6a575792a7684e8f2
GAOGA4 <961977096@qq.com>
2020-08-13 11:52:13 +0800

    [ST10][Gavin] bug: fix reservationEndTime

--- src/components/BookOrder/index.jsx
+++ src/components/BookOrder/index.jsx
@@ -61,7 +61,7 @@ class BookOrder extends React.Component {
                           {moment(bookOrder.reservationStartTime).format(
                             'YYYY-MM-DD HH:mm').toLocaleString()}
                           {' - '}
-                          {moment(bookOrder.reservationEndTimenew).format(
+                          {moment(bookOrder.reservationEndTime).format(
                             'YYYY-MM-DD HH:mm').toLocaleString()}
                         </Descriptions.Item>
 
